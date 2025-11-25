232b5455b2723aae624070915c71efc7e60fc08f
Easwar H <easwarharikaran1610@gmail.com>
2025-11-25 11:56:23 +0530

    postgres connectivity and SQL injection and best practices

---
+++ dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const pgClient = new pg_1.Client("postgresql://neondb_owner:npg_gLVyHhmwG0Q9@ep-polished-king-a4froz22-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
+pgClient.connect();
+app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    try {
+        const user = yield pgClient.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`, [username, email, password]);
+        return res.status(200).json({ message: "User Created successfully" });
+    }
+    catch (err) {
+        return res.status(500).json({ error: err });
+    }
+}));
+app.listen(3000, () => {
+    console.log("Listening on 3000");
+});
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { Client } from "pg";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const pgClient = new Client(
+  "postgresql://neondb_owner:npg_gLVyHhmwG0Q9@ep-polished-king-a4froz22-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+);
+
+pgClient.connect();
+
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  try {
+    const user = await pgClient.query(
+      `INSERT INTO users (username, email, password) VALUES ($1, $2, $3);`,
+      [username, email, password]
+    );
+    return res.status(200).json({ message: "User Created successfully" });
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
